01ae35b31379f617960d13897603cc2645f68e02
Samuel Hafen <samuel.hafen@aol.com>
2025-02-03 22:48:09 +0100

    adding comments to files app

--- files/main.js
+++ files/main.js
@@ -12,6 +12,7 @@ if (rustdrive.standalone) {
 
 class Path {
 	constructor(input) {
+		// Initialize list from input string or array
 		if (typeof input === 'string') {
 			this.list = input.split('/').filter(s => s !== '');
 		} else {
@@ -20,20 +21,24 @@ class Path {
 	}
 
 	as_list() {
+		// Return the path as a list
 		return this.list;
 	}
 
 	as_string() {
+		// Join the list into a string separated by '/'
 		return this.list.join('/');
 	}
 
 	as_strings() {
+		// Return a list of strings showing the path at each depth
 		return this.list.map((item, index) => {
 			return this.list.slice(0, index + 1).join('/');
 		});
 	}
 
 	go_deeper(subPath) {
+		// Add subPath to the current path
 		if (typeof subPath === 'string') {
 			this.list = this.list.concat(subPath.split('/').filter(s => s !== ''));
 		} else if (Array.isArray(subPath)) {
@@ -42,6 +47,7 @@ class Path {
 	}
 
 	go_up(level = 1) {
+		// Remove the specified number of levels from the end of the path
 		this.list = this.list.slice(0, Math.max(0, this.list.length - level));
 	}
 }
@@ -52,43 +58,46 @@ var files;
 // Build Filelist
 async function updateFiles() {
 
-	var breadcrumbs = document.getElementById("breadcrumbs");
-	var template = document.getElementById("breadcrumb-template");
-	
-	breadcrumbs.innerHTML = "";
+	// Get breadcrumb and template elements
+    var breadcrumbs = document.getElementById("breadcrumbs");
+    var template = document.getElementById("breadcrumb-template");
+    
+    // Clear existing breadcrumbs
+    breadcrumbs.innerHTML = "";
 
-	var li = template.content.querySelector("li").cloneNode(true);
-	li.querySelector("a").textContent = "home";
-	li.onclick = () => setPath([]);
-	breadcrumbs.appendChild(li);
+    // Add "home" breadcrumb
+    var li = template.content.querySelector("li").cloneNode(true);
+    li.querySelector("a").textContent = "home";
+    li.onclick = () => setPath([]);
+    breadcrumbs.appendChild(li);
 
-	for ( const directory in path.as_list() ) {
-		var li = template.content.querySelector("li").cloneNode(true);
-		li.querySelector("a").textContent = path.as_list()[directory];
-		li.onclick = () => setPath(path.as_list().slice(0, directory + 1));
-		breadcrumbs.appendChild(li);
-	}
-	
+    // Add breadcrumbs for each directory in the path
+    for (const directory in path.as_list()) {
+        var li = template.content.querySelector("li").cloneNode(true);
+        li.querySelector("a").textContent = path.as_list()[directory];
+        li.onclick = () => setPath(path.as_list().slice(0, directory + 1));
+        breadcrumbs.appendChild(li);
+    }
+    
+    // Fetch the list of files from the API
+    files = await rustdrive.api.file_list(path.as_string()); 
+    var filelist = document.getElementById("filelist");
+    var template = document.getElementById("file-template");
 
-	files = await rustdrive.api.file_list(path.as_string()); 
-	var filelist = document.getElementById("filelist");
-	var template = document.getElementById("file-template");
+    // Clear existing file list
+    filelist.innerHTML = "";
 
-	filelist.innerHTML = "";
-
-	for ( const file in files ) {
-		if (files[file].folder) {
-			var li = template.content.querySelector("a").cloneNode(true);
-			li.textContent = files[file].name;
-			li.ondblclick = () => openFolder(files[file].name);
-			filelist.appendChild(li);
-		} else {
-			var li = template.content.querySelector("a").cloneNode(true);
-			li.textContent = files[file].name;
-			li.ondblclick = () => openFile(files[file].name);
-			filelist.appendChild(li);
-		}
-	}
+    // Add each file or folder to the file list
+    for (const file in files) {
+        var li = template.content.querySelector("a").cloneNode(true);
+        li.textContent = files[file].name;
+        if (files[file].folder) {
+            li.ondblclick = () => openFolder(files[file].name);
+        } else {
+            li.ondblclick = () => openFile(files[file].name);
+        }
+        filelist.appendChild(li);
+    }
 
 }
 
